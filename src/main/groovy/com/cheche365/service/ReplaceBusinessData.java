17c9d237c740be0761993f5e00a884979bc10da3
cheche-wzl <wangzl@cheche365.com>
2020-04-23 00:37:27 +0800

    fix(1.1.1.2): 替换数据完成最后一步更新bug
    reviewer:WZL

--- src/main/groovy/com/cheche365/service/ReplaceBusinessData.java
+++ src/main/groovy/com/cheche365/service/ReplaceBusinessData.java
@@ -78,7 +78,7 @@ public class ReplaceBusinessData {
             "         where result_table_name = 'resultTableNameVal'\n" +
             "         ) t4\n" +
             "                 on t1.id = t4.result_id\n" +
-            "       where handle_sign in (3,6) \n" +
+            "       where handle_sign = 3 \n" +
             "       and `8-险种名称` in ('交强险', '商业险') and `9-保单出单日期` >= '2019-01-01' \n" +
             "       and t4.id is null";
 
@@ -515,13 +515,13 @@ public class ReplaceBusinessData {
         return "'" + obj.toString() + "'";
     }
 
-    private static String generateSettlementTableName(String tableName) {
-        String settlementTableName = "settlement_" + tableName.substring(7);
+    private String generateSettlementTableName(String tableName) {
+        String settlementTableName = "settlement_" + tableName.substring(0, tableName.lastIndexOf("_"));
         return settlementTableName;
     }
 
     private String generateCommissionTableName(String tableName) {
-        String commissionTableName = "commission_" + tableName.substring(7);
+        String commissionTableName = "commission_" + tableName.substring(0, tableName.lastIndexOf("_"));
         return commissionTableName;
     }
 
